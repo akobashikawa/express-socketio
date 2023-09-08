5dec3b2ba7d11c34aaf267901a46ebd53dd6da12
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-09-08 17:11:24 -0500

    Separando server.js

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
-const http = require('http');
 const path = require('path');
 const cors = require('cors');
-const { configureSocket } = require('./api/modules/sockets/SocketsManager');
 
-const port = process.env.PORT || '3000';
 const app = express();
-server = http.createServer(app);
-const io = configureSocket(server);
-app.set('io', io);
 
 app.use(cors());
 app.use(express.json());
@@ -23,16 +17,5 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api/sockets', socketsRouter);
 app.use('/api/tareas', tareasRouter);
-
-server.listen(port);
-
-server.on('listening', () => {
-  console.log(`Express corriendo en puerto ${server.address().port}`);
-});
-
-server.on('error', (error) => {
-  console.error(error);
-  throw error;
-});
 
 module.exports = app;
